08527bb756ec8102e4da8e9ec6bb30f400e10b2b
Daryll Netscher <DaryllxNetscher@asfs-MacBook.local>
2015-08-30 03:07:17 +0100

    Initial Commit

---
+++ Zig-Zag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zig-Zag
+//
+//  Created by Daryll Netscher on 30/08/2015.
+//  Copyright (c) 2015 Netscher Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
